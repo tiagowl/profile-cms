01e66c6ea953eb3269250aa1102bdbd1791900ff
tiagowl <winkellandi123@gmail.com>
2023-01-30 12:47:05 -0300

    funcionalidade de like no comentário dos posts

--- src/api/comment/controllers/comment.ts
+++ src/api/comment/controllers/comment.ts
@@ -4,4 +4,14 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::comment.comment');
+export default factories.createCoreController('api::comment.comment', ({strapi})=>({
+
+    async update(ctx){
+        const body = ctx.request.body.data;
+
+        if(body.likes){
+            await strapi.service('api::comment.comment').like(ctx.request.params.id);
+        }
+    }
+
+}));
--- src/api/comment/services/comment.ts
+++ src/api/comment/services/comment.ts
@@ -3,5 +3,19 @@
  */
 
 import { factories } from '@strapi/strapi';
+import like from '../../like/controllers/like';
 
-export default factories.createCoreService('api::comment.comment');
+export default factories.createCoreService('api::comment.comment', ({strapi})=>({
+    
+    async like(commentId){
+        const likes = await strapi.entityService.findOne('api::comment.comment', commentId, {});
+        const commentWithLikes = {...likes, likes: likes.likes++};
+        console.log(commentWithLikes);
+        const updateComment = await strapi.entityService.update('api::comment.comment', commentId,{
+            fields: ['likes'],
+            data: {...likes, likes: likes.likes++}
+        });
+        return updateComment;
+    }
+
+}));
