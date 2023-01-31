753fb2b80431406359a631414e37f6b1a8620b52
tiagowl <winkellandi123@gmail.com>
2023-01-31 10:31:44 -0300

    likes nos subcomentários

--- src/api/sub-comment/controllers/sub-comment.ts
+++ src/api/sub-comment/controllers/sub-comment.ts
@@ -4,4 +4,14 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::sub-comment.sub-comment');
+export default factories.createCoreController('api::sub-comment.sub-comment', ({strapi})=>({
+
+    async update(ctx){
+        const body = ctx.request.body.data;
+
+        if(body.likes){
+            await strapi.service('api::sub-comment.sub-comment').like(ctx.request.params.id);
+        }
+    }
+
+}));
--- src/api/sub-comment/services/sub-comment.ts
+++ src/api/sub-comment/services/sub-comment.ts
@@ -4,4 +4,17 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreService('api::sub-comment.sub-comment');
+export default factories.createCoreService('api::sub-comment.sub-comment', ({strapi})=>({
+
+    async like(commentId){
+        const likes = await strapi.entityService.findOne('api::sub-comment.sub-comment', commentId, {});
+        const commentWithLikes = {...likes, likes: likes.likes++};
+        console.log(commentWithLikes);
+        const updateComment = await strapi.entityService.update('api::sub-comment.sub-comment', commentId,{
+            fields: ['likes'],
+            data: {...likes, likes: likes.likes++}
+        });
+        return updateComment;
+    }
+
+}));
